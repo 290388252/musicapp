15ce8222d2d0f2bd1f66a810d9a481c32742dfcf
ychao <290388252@qq.com>
2017-12-26 17:17:39 +0800

    jsonp fixed slider created

--- src/api/recommend.js
+++ src/api/recommend.js
@@ -2,7 +2,7 @@
  * Created by Administrator on 2017/12/26.
  */
 import jsonp from '../common/js/jsonp';
-import {commonParams, options} from 'config';
+import {commonParams, options} from './config';
 
 export function getRecommend() {
   const url = 'https://c.y.qq.com/musichall/fcgi-bin/fcg_yqqhomepagerecommend.fcg';
@@ -11,6 +11,6 @@ export function getRecommend() {
     platform: 'h5',
     uin: 0,
     needNewCode: 1
-  })
-  return jsonp(url, data, options)
+  });
+  return jsonp(url, data, options);
 };
--- src/common/js/jsonp.js
+++ src/common/js/jsonp.js
@@ -1,10 +1,10 @@
 /**
  * yanchao
- * Created by Administrator on 2017/12/26.
+ * Created by yanchao on 2017/12/26.
  */
 import originJSONP from 'jsonp';
-url += (url.indexOf('?') < 0 ? '?' : '&') + param(data);
 export default function jsonp(url, data, option) {
+  url += (url.indexOf('?') < 0 ? '?' : '&') + param(data);
   return new Promise((resolve, reject) => {
       originJSONP(url, option, (err, data) => {
         if (!err) {
@@ -12,15 +12,15 @@ export default function jsonp(url, data, option) {
         } else {
           reject(err);
         }
-      })
+      });
   });
 };
 
 function param(data) {
   let url = '';
-  for (var i in data) {
+  for (let i in data) {
     let value = data[i] !== undefined ? data[i] : '';
-    url += `&${i}=${encodeURIComponent(value)}`
+    url += `&${i}=${encodeURIComponent(value)}`;
   }
   return url ? url.substring(1) : '';
-};
+}
