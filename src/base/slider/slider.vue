f314fcee5055e8ee5c08f67d4a6b3a6dc9193e41
ychao <290388252@qq.com>
2017-12-26 11:58:19 +0800

    jsonp recommend api

---
+++ src/base/slider/slider.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="slider">
+      <div class="slider-group">
+        <slot></slot>
+      </div>
+      <div class="dots">
+      </div>
+    </div>
+</template>
+
+<style lang="stylus" rel="stylesheet/stylus">
+</style>
+
+<script type="text/ecmascript-6">
+    export default{};
+</script>
