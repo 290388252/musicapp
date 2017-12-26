727ddc134d747ec1538fbbe4ea0defd97a29cf67
ychao <290388252@qq.com>
2017-12-26 10:35:56 +0800

    project framework created [recommend, search, singer, rank, tab]

---
+++ src/components/rank/rank.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+      排行
+    </div>
+</template>
+
+<style lang="stylus" rel="stylesheet/stylus">
+</style>
+
+<script type="text/ecmascript-6">
+    export default{};
+</script>
---
+++ src/components/search/search.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+      搜索
+    </div>
+</template>
+
+<style lang="stylus" rel="stylesheet/stylus">
+</style>
+
+<script type="text/ecmascript-6">
+    export default{};
+</script>
