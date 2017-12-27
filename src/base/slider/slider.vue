329f6a95d39228f428c73b231bfd1a2b8acd86da
ychao <290388252@qq.com>
2017-12-27 11:57:09 +0800

    [slider] fixed auto slide and move slide

--- src/base/slider/slider.vue
+++ src/base/slider/slider.vue
@@ -4,13 +4,16 @@
         <slot></slot>
       </div>
       <div class="dots">
+        <span class="dot" v-for="(item, index) in dots" :class="{active: currentIndex === index}"></span>
       </div>
     </div>
 </template>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  @import '../../common/stylus/variable.styl';
   .slider
     min-height 1px
+    position relative
     .slider-group
       position relative
       overflow hidden
@@ -28,12 +31,37 @@
         img
           display: block
           width: 100%
+    .dots
+      position absolute
+      right 0
+      left 0
+      bottom 8px
+      text-align center
+      font-size 0
+      .dot
+        display inline-block
+        background $color-text-l
+        border-radius: 50%
+        margin-right 3px
+        width: 8px
+        height: 8px
+        &.active
+          width: 15px
+          border-radius: 5px
+          background: $color-text-ll
 </style>
 
 <script type="text/ecmascript-6">
   import BScroll from 'better-scroll';
   import {addClass} from '../../common/js/dom';
     export default{
+        data() {
+            return {
+              dots: [],
+              currentIndex: 0,
+              childrenlength: 0
+            };
+        },
         props: {
             loop: {
                 type: Boolean,
@@ -45,18 +73,23 @@
             },
             interval: {
               type: Number,
-              default: 2000
+              default: 1500
             }
         },
         mounted() {
             setTimeout(() => {
                 this._setSliderWidth();
+                this._initDots();
                 this._initSlider();
+                if (this.autoPlay) {
+                  this._play();
+                }
             }, 20);
         },
         methods: {
             _setSliderWidth() {
               this.children = this.$refs.sliderGroup.children;
+              this.childrenlength = this.children.length;
               let width = 0;
               let sliderWidth = this.$refs.slider.clientWidth;
               for (let i = 0; i < this.children.length; i++) {
@@ -73,13 +106,44 @@
             _initSlider() {
               this.slider = new BScroll(this.$refs.slider, {
                 scrollX: true,
-                scrollY: true,
+                scrollY: false,
                 momentum: false,
-                snap: true,
-                snapLoop: this.loop,
-                snapThreshold: 0.3,
-                snapSpeed: 400
+//                新版本的Better-Scroll
+                snap: {
+                    loop: this.loop,
+                    threshold: 0.3,
+                    speed: 400
+                },
+//                旧版本的Better-Scroll0.1.15
+//                snapLoop: this.loop,
+//                snapThreshold: 0.3,
+//                snapSpeed: 400,
+                click: true
               });
+
+              this.slider.on('scrollEnd', () => {
+                  let pageIndex = this.slider.getCurrentPage().pageX;
+                  if (this.loop) {
+                    this.currentIndex = pageIndex;
+                  }
+              });
+            },
+            _initDots() {
+                this.dots = new Array(this.children.length);
+            },
+            _play() {
+              let pageIndex = this.currentIndex;
+//              if (this.loop) {
+//                pageIndex += 1;
+//              }
+              this.timer = setInterval(() => {
+                  this.slider.goToPage(pageIndex, 0, 400);
+                  if (pageIndex >= this.childrenlength) {
+                      pageIndex = 0;
+                  } else {
+                      pageIndex += 1;
+                  }
+              }, this.interval);
             }
         }
     };
