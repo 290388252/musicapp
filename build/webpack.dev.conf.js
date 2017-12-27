875752ef39a98c9cb127a34f861efb25583675ae
ychao <290388252@qq.com>
2017-12-27 17:10:59 +0800

    [webpack.dev.conf.js] catch api getDiscList

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -13,6 +13,23 @@ const PORT = process.env.PORT && Number(process.env.PORT);
 const app = express();
 const express = require('express');
 const apiRoutes = express.Router();
+const axios = require('axios');
+
+apiRoutes.get('/getDiscList', function (req, res) {
+  const url = 'https://c.y.qq.com/splcloud/fcgi-bin/fcg_get_diss_by_tag.fcg';
+  axios.get(url, {
+    headers: {
+      referer: 'https://c.y.qq.com/',
+      host: 'c.y.qq.com'
+    },
+    params: req.query
+  }).then((response) => {
+    res.json(response.data)
+  }).catch((e) => {
+    console.log(e)
+  });
+});
+
 const devWebpackConfig = merge(baseWebpackConfig, {
   module: {
     rules: utils.styleLoaders({ sourceMap: config.dev.cssSourceMap, usePostCSS: true })
