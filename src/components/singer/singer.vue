10add7d8cf9af2637df4924210bcdad9d20789aa
yanchao <290388252@qq.com>
2017-12-28 23:18:53 +0800

    getSingerList api test

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -8,5 +8,27 @@
 </style>
 
 <script type="text/ecmascript-6">
-    export default{};
+  import {getSingerList} from '../../api/singer';
+  import {ERR_OK} from '../../api/config';
+
+    export default{
+      data() {
+        return {
+          singerList: []
+        };
+      },
+      created() {
+        this._getSingerList();
+      },
+      methods: {
+        _getSingerList() {
+          getSingerList().then((res) => {
+            if (res.code === ERR_OK) {
+              this.singerList = res.data.list;
+              console.log(this.singerList);
+            }
+          });
+        }
+      }
+    };
 </script>
