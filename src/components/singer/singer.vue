1919c753e0af0798e460b363126e47b6873f4095
ychao <290388252@qq.com>
2017-12-29 15:19:34 +0800

    [singer.vue] optimize getSingerList api Array title{hot, A ~ Z}

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,15 +1,22 @@
 <template>
-    <div>
-      歌手
+    <div class="singer">
     </div>
 </template>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/variable.styl"
+  .singer
+    display fixed
+    width 100%
+    height 100%
 </style>
 
 <script type="text/ecmascript-6">
   import {getSingerList} from '../../api/singer';
   import {ERR_OK} from '../../api/config';
+
+  let HOT_NAME = '热门';
+  let HOT_SINGER_LENGTH = 10;
 
     export default{
       data() {
@@ -25,9 +32,39 @@
           getSingerList().then((res) => {
             if (res.code === ERR_OK) {
               this.singerList = res.data.list;
-              console.log(this.singerList);
+              console.log(this._normalizeSinger(this.singerList));
             }
           });
+        },
+        _normalizeSinger(list) {
+            let map = {
+              hot: {
+                  title: HOT_NAME,
+                  item: []
+              }
+            };
+            list.forEach((item, index) => {
+                if (index < HOT_SINGER_LENGTH) {
+                    map.hot.item.push({
+                        id: item.Fsinger_id,
+                        name: item.Fsinger_name,
+                        avatar: `//y.gtimg.cn/music/photo_new/T001R150x150M000${item.Fsinger_mid}.jpg?max_age=2592000`
+                    });
+                }
+                const key = item.Findex;
+                if (!map[key]) {
+                    map[key] = {
+                      title: key,
+                      item: []
+                    };
+                }
+                map[key].item.push({
+                  id: item.Fsinger_id,
+                  name: item.Fsinger_name,
+                  avatar: `//y.gtimg.cn/music/photo_new/T001R150x150M000${item.Fsinger_mid}.jpg?max_age=2592000`
+                });
+            });
+            return map;
         }
       }
     };
