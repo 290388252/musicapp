dcfb34ddcb8330513d604580ec7ae956a6219dce
ychao <290388252@qq.com>
2018-01-05 15:11:41 +0800

    [changed] actions select play

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,4 +1,13 @@
 /**
- *
+ *yanchao
  * Created by Administrator on 2018/1/3.
  */
+import * as types from './mutations-type';
+
+export const selectPlay = function ({commit, state}, {list, index}) {
+  commit(types.SET_PLAYING_LIST, list);
+  commit(types.SET_SEQUENCE_LIST, list);
+  commit(types.SET_CURRENT_INDEX, index);
+  commit(types.SET_FULL_SCREEN, true);
+  commit(types.SET_PLAYING_STATE, true);
+};
