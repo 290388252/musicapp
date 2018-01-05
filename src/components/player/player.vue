3c93765c1f40adb1aafd72fe2e219e8e139fe89f
ychao <290388252@qq.com>
2018-01-05 14:58:54 +0800

    [new] created player.vue

---
+++ src/components/player/player.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="player" v-show="playList.length>0">
+      <div class="normal-player" v-show="fullScreen">
+        播放器
+      </div>
+      <div class="mini-player" v-show="!fullScreen"></div>
+    </div>
+</template>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/variable.styl"
+  @import "../../common/stylus/mixin.styl"
+
+  .player
+    .normal-player
+      position: fixed
+      left 0
+      right 0
+      top 0
+      bottom 0
+      z-index 150
+      background: $color-background
+</style>
+
+<script type="text/ecmascript-6">
+  import {mapGetters} from 'vuex';
+
+    export default{
+      computed: {
+        ...mapGetters([
+          'fullScreen',
+          'playList'
+        ])
+      }
+    };
+</script>
