68d651484e64ae5daaae22fed6be3e706f684ab5
ychao <290388252@qq.com>
2018-01-09 17:34:45 +0800

    [Changed] player.vue

--- src/common/js/songs.js
+++ src/common/js/songs.js
@@ -24,7 +24,8 @@ export function createSong(musicData) {
     album: musicData.albumname,
     duration: musicData.interval,
     image: `https://y.gtimg.cn/music/photo_new/T002R300x300M000${musicData.albummid}.jpg?max_age=2592000`,
-    url: `http://ws.stream.qqmusic.qq.com/${musicData.songid}.m4a?fromtag=46`
+    // url: `http://ws.stream.qqmusic.qq.com/${musicData.songid}.m4a?fromtag=46`
+    url: `http://dl.stream.qqmusic.qq.com/C400${musicData.songmid}.m4a?vkey=4D2B651E60EC3EA3B623382755B569CDC2BB055D2F7C82B1AFF8A92D3DD8F9FEBA4A99DC35B43965C8784FFC147F19DE4982F0B973054AC5&guid=7342625256&uin=0&fromtag=66`
   });
 }
 
