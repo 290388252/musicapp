74c750dad49b1a40acd51e1c2542ba11d8d8fa01
ychao <290388252@qq.com>
2018-01-10 17:15:09 +0800

    [Changed] progress-bar.vue

--- src/base/progress-bar/progress-bar.vue
+++ src/base/progress-bar/progress-bar.vue
@@ -1,9 +1,27 @@
 <template>
-    <div>
+    <div class="progress-bar">
+      <div class="bar-inner">
+        <div class="progress"></div>
+        <div class="progress-btn-wrapper">
+          <div class="progress-btn">
+          </div>
+        </div>
+      </div>
     </div>
 </template>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  .progress-bar
+    height: 30px
+    .bar-inner
+      position: relative
+      top: 13px
+      height: 4px
+      background: rgba(0, 0, 0, 0.3)
+      .progress
+        position: absolute
+        height: 100%
+        background: $color-theme
 </style>
 
 <script type="text/ecmascript-6">
