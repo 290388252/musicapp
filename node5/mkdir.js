33cf8bd75015ffb4d839c0a88c6c74701e5a16aa
ychao <290388252@qq.com>
2018-02-11 17:08:20 +0800

    [Node] music

--- node5/mkdir.js
+++ node5/mkdir.js
@@ -2,17 +2,18 @@ const fs = require('fs');
 const path = require('path');
 
 function mkdir(pathname,callback){
-	pathname = path.isAbsolute(pathname) ? pathname : path.join(__dirname,pathname);
+  let root = module.parent.filename;
+	pathname = path.isAbsolute(pathname) ? pathname : path.join(root,pathname);
 	//pathname.replace(__dirname, '');
-  let relativepath = path.relative(__dirname,pathname);
+  let relativepath = path.relative(root,pathname);
   let folders = relativepath.split(path.sep); //path.sep当前系统下的分割符号，考虑到windows和linux
 	try{
     let pre = '';
     folders.forEach(folder=>{
       try {
-        fs.statSync(path.join(__dirname,pre,folder));
+        fs.statSync(path.join(root,pre,folder));
       }catch (err){
-        fs.mkdirSync(path.join(__dirname,pre,folder));
+        fs.mkdirSync(path.join(root,pre,folder));
       }
       pre = path.join(pre,folder);
     });
